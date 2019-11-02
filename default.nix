297bfac157736c617b8bfc3c274141bc65b73d3f
Malte Brandy <malte.brandy@maralorn.de>
2019-11-02 02:31:57 +0100

    Fix warnings

---
+++ default.nix
@@ -0,0 +1,21 @@
+{ pkgs ? import <unstable> { } }:
+
+let
+  haskellPackages = pkgs.haskellPackages;
+  drv = haskellPackages.callCabal2nix "taskwarrior" ./. { };
+
+in {
+  taskwarrior = drv;
+  shell = haskellPackages.shellFor {
+    withHoogle = true;
+    packages = p: [ drv ];
+    buildInputs = with haskellPackages; [
+      hlint
+      cabal-install
+      brittany
+      haskell-ci
+      shake
+      pkgs.coreutils
+    ];
+  };
+}
